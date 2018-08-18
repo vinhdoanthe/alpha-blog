b777c33200886d1a433cf7895434d20ba307293f
Vinh <doanthevinh1991@gmail.com>
2018-08-18 17:37:40 +0000

    finish lesson 65

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,25 @@ class ArticlesController < ApplicationController
    def new
        @article = Article.new
    end
-    
+   
+   def create
+      @article = Article.new(article_params)
+      # @article.save
+      # redirect_to articles_path(@article)
+      if @article.save
+         flash[:notice] = "Article created successfully"
+         redirect_to article_path(@article)
+      else
+         render :new
+      end
+   end
+   
+   def show
+      @article = Article.find(params[:id])
+   end
+   
+   private
+   def article_params
+      params.require(:article).permit(:title, :description)
+   end
 end
