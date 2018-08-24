93d1450ce0784658f7e7a8b0e6dc484814a9befc
Vinh <doanthevinh1991@gmail.com>
2018-08-24 07:59:39 +0000

    Validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
     
+    validates :username, presence: true, 
+              uniqueness: {case_sensitive: false}, 
+              length: {minimum: 3, maximum: 25}
+    
+    VALID_EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i
+    validates :email, presence: true,
+              length: {maximum: 105},
+              uniqueness: {case_sensitive: false},
+              format: {with: VALID_EMAIL_REGEX}
     
 end
